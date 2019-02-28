abe8485042a9e203fde7169512badd551c1c5c21
ejsantos <ejsantos@DSK-0110>
2019-02-28 15:21:09 -0300

    Add project files.

---
+++ TesteVs2015GitHub/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteVs2015GitHub
+{
+    public static class ConversorTemperatura
+    {
+        public static double FahrenheitParaCelsius(double temperatura) { return 0; }
+
+        public static double FahrenheitParaKelvin(double temperatura) { return 0; }
+    }
+}
